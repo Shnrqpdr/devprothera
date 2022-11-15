2764e4d251887e9195c7cff5b9524224ac982fb3
felipesd1 <felipe.damasceno@prothera.com.br>
2022-11-15 11:03:07 -0300

    Inserindo lib easy-data-table

--- views/pages/Traducoes.js
+++ views/pages/Traducoes.js
@@ -1,4 +1,5 @@
 import ModalTraducao from '/components/ModalTraducao.js';
+import Tabela from '/components/Tabela.js';
 
 const Traducoes = async () => {
   let template = await fetch("/pages/Traducoes.html")
@@ -7,13 +8,24 @@ const Traducoes = async () => {
   return ({
     template: template,
     components: {
-      "ModalTraducao": await ModalTraducao()
+      "ModalTraducao": await ModalTraducao(),
+      "Tabela": await Tabela(),
     },
     data() {
       return {
         traducoes: [],
         novasTraducoes: {},
-        chavesModificadas: {}
+        chavesModificadas: {},
+        tabela: {
+          columns: [
+            { label: 'ID', field: 'id', align: 'center', filterable: false },
+            { label: 'Username', field: 'user.username' },
+            { label: 'First Name', field: 'user.first_name' },
+            { label: 'Last Name', field: 'user.last_name' },
+            { label: 'Email', field: 'user.email', align: 'right', sortable: false }
+          ],
+          rows: window.rows
+        }
       }
     },
     mounted() {
